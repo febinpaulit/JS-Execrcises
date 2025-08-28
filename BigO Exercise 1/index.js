9dd2a63594258c9fb90b06d58fe34e45257bf7e5
febin paul <febinavsilu@gmail.com>
2025-08-28 22:10:10 +0530

    test

---
+++ BigO Exercise 1/index.js
@@ -0,0 +1,14 @@
+function funChallenge(input) {
+    let a = 10; //O(1)
+    a = 50 + 3; //O(1)
+    for (let i = 0; i < input.length; i++) { //O(n)
+        anotherFunction();//O(n)
+        let starnger = true;//O(n)
+        a++;//O(n)
+    }
+    return a;//O(1)
+}
+
+funChallenge();
+
+// O(3+4n)
---
+++ First/main.js
@@ -0,0 +1,18 @@
+const nemo = ['sdf','sfddf','sdfsdgts','nemo','sdf','sdfsda','styey','sdg','dfsgs','sdfg'];
+const large = new Array(100000).fill('nemo');
+function findNemo(array){
+    
+    let t0 = performance.now();
+    
+    for(let i=0;i<array.length;i++){
+        if(array[i] == 'nemo'){
+            console.log('Found NEMO!');
+        }
+    }
+    
+    let t1 = performance.now();
+    
+    console.log('Call to find Nemo took '+(t1-t0)+' milliseconds');
+}
+
+findNemo(large);
